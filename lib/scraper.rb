741b5e31d9fca83a8cc3e43e7cd04131daa43b7a
Persi87 <simon_black87@hotmail.co.uk>
2022-01-19 00:29:28 +0000

    commit tree 835de7801572347c917be81a4db6f019853084c4

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,56 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = URI.open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    student_index_array = []
+
+      doc.css(".student-card a").each do |student|
+        # student_name = student.children.children.children[1].text
+        student_name = student.css(".student-name").text
+        student_location = student.css(".student-location").text
+        student_url = student.attribute('href').value # gone through the returned array rather than the css here.  Worked way through the array and hashes
+        # student_url = student.css("a")[0].attributes["href"].value #use the "students.css" array output to help locate the needed code, and work through the levels
+        student_index_array << {name: student_name, location: student_location, profile_url: student_url}
+        # binding.pry
+      end
+
+      student_index_array
+
   end
 
   def self.scrape_profile_page(profile_url)
+    html = URI.open(profile_url)
+    doc = Nokogiri::HTML(html)
+
+    profile = {}
+
+    quote = doc.css(".profile-quote").text
+    profile[:profile_quote] = quote
+
+    bio = doc.css(".description-holder p").text
+    profile[:bio] = bio
+
+    profile_links = doc.css(".social-icon-container a").collect {|link_array| link_array.attributes["href"].value}
+    
+    profile_links.each do |link|
+      if link.include?("twitter.com")
+        profile[:twitter] = link
+      elsif link.include?("linkedin.com")
+        profile[:linkedin] = link
+      elsif link.include?("github.com")
+        profile[:github] = link
+      else
+        profile[:blog] = link
+      end
+    end
+
+    profile
     
   end
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,29 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)   # have a look at this coz I don't really understand it... "Mass Assignment"
+    end
+    @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students_array) #passing in - [{name: Simon, location: Streetly}, {another:, hash}] 
+    students_array.each do |student_info|
+         Student.new(student_info)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+    self.name
   end
 
   def self.all
-    
+    @@all
   end
 end
 
